a50f4743a2d8fdfc9fad7997f08e659a150bdd56
AprilHall <abigailjohanna@yahoo.com>
2017-11-05 17:25:59 -0500

    second commit

--- fifteen.js
+++ fifteen.js
@@ -85,3 +85,116 @@ window.onload = function()
     }
   }
 }
+
+function swap(pos)
+{
+  var temp = pieces[pos].style.top;
+  pieces[pos].style.top = posY;
+  posY = temp;
+  temp = pieces[pos].style.left;
+  pieces[pos].style.left = posX;
+  posX = temp;
+}
+
+function moveLeft(x, y)
+{
+  var xx = parseInt(x);
+  var yy = parseInt(y);
+
+  if (xx > 0)
+  {
+    for (var m = 0; m < pieces.length; m++)
+    {
+      if (parseInt(pieces[m].style.left) + 100 == xx && parseInt(pieces[m].style.top) == yy)
+      {
+        return m;
+      }
+    }
+  }
+  else
+  {
+    return -1;
+  }
+}
+
+function moveRight(x, y)
+{
+  var xx = parseInt(x);
+  var yy = parseInt(y);
+  if (xx < 300)
+  {
+    for (var n =0; n < pieces.length; n++)
+    {
+      if (parseInt(pieces[n].style.left) - 100 == xx && parseInt(pieces[n].style.top) == yy)
+      {
+        return n;
+      }
+    }
+  }
+  else
+  {
+    return -1;
+  }
+}
+
+function moveUp(x, y)
+{
+  var xx = parseInt(x);
+  var yy = parseInt(y);
+  if (yy > 0)
+  {
+    for (var o = 0; o < pieces.length; o++){
+      if (parseInt(pieces[o].style.top) + 100 == yy && parseInt(pieces[o].style.left) == xx)
+      {
+        return o;
+      }
+    }
+  }
+  else
+  {
+    return -1;
+  }
+}
+
+function moveDown(x, y)
+{
+  var xx = parseInt(x);
+  var yy = parseInt(y);
+  if (yy < 300)
+  {
+    for (var p = 0; p < pieces.length; p++)
+    {
+      if (parseInt(pieces[p].style.top) - 100 == yy && parseInt(pieces[p].style.left) == xx)
+      {
+        return p;
+      }
+    }
+  }
+  else
+  {
+    return -1;
+  }
+}
+
+function moveCheck(pos)
+{
+  if (moveLeft(posX, posY) == (pos-1))
+  {
+    return true;
+  }
+
+  if (moveDown(posX, posY) == (pos-1))
+  {
+    return true;
+  }
+
+  if (moveUp(posX, posY) == (pos-1))
+  {
+    return true;
+  }
+
+  if (moveRight(posX, posY) == (pos-1))
+  {
+    return true;
+  }
+}
